b060e72e175773354a7896c2ef72d7b0515fcb18
birghans <75995867+birghans@users.noreply.github.com>
2020-12-19 13:47:39 +0100

    Update mail_me.php

--- mail_me.php
+++ mail_me.php
@@ -1,23 +1,20 @@
 <?php
 
-  if(isset($_POST['submit'])){
-      $fname=$_POST['fname'];
-      $lname=$_POST['lname'];
-      $email=$_POST['email'];
-      $phone=$_POST['phone'];
-      $msg=$_POST['msg'];
+  if (isset($_POST['submit'])){
+      $fname = $_POST['fname'];
+      $lname = $_POST['lname'];
+      $email = $_POST['email'];
+      $phone = $_POST['phone'];
+      $msg = $_POST['msg'];
 
-      $to='birghan.sven@web.de';
-      $subject='Form Submission';
-      $message="Name: ".$fname."\n"."Phone: ".$phone."\n". "Wrote the following: "."\n\n" .$msg;
-      $headers= "From: ".$email;
+      $to = 'birghan.sven@web.de';
+      $subject = 'Form Submission';
+      $message = "Name: ".$fname.".\n"."Phone: ".$phone."\n". "Wrote the following: "."\n\n" .$msg;
+      $headers = "From: ".$email;
 
-      if(mail($to, $subject, $message, $headers)){
-        echo "<h1>Hallo ".$fname.", danke für Deine Nachricht!</h1>";
-      }
-      else{
-        echo "Etwas ist schief gelaufen :("
-      }
-    }
+      mail($to, $subject, $message, $headers);
+      header("Location: kontakt.html?mailsend");
+
+  }
 
 ?>
