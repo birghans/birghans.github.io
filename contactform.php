9dddc9c056f04644072e87c18353377d42c1dbf9
birghans <75995867+birghans@users.noreply.github.com>
2020-12-27 16:45:00 +0100

    New

--- contactform.php
+++ contactform.php
@@ -10,10 +10,24 @@
 
     $mailTo = "birghan.sven@web.de";
     $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$fname.".\n\n".$message;
+    $txt = "You have received an e-mail from ".$fname.".\n\n".$lname.".\n\n".$message;
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: kontakt.html?mailsend");
+    /*if(empty($_POST['fname'])) {
+      $fname = "";
+    }
+
+    if(empty($_POST['lname'])) {
+      $lname = "";
+    }
+
+    if(empty($_POST['email'])) {
+      echo "Bitte tragen Sie vor dem Absenden Ihre E-Mail Adresse ein!";
+    } else {*/
+      mail($mailTo, $subject, $txt, $headers);
+      /*header("Location: kontakt.html?mailsend");*/
+      /*echo "Ihre Anfrage wurde erfolgreich übermittelt";
+    }*/
+
   }
 
  ?>
